3bd9db54f6cbae3ed53f66a74b074c8e3df72802
ShizuR <raishizu@hotmail.com>
2026-04-01 20:29:25 +0100

    see shape

--- extract_feature.py
+++ extract_feature.py
@@ -92,7 +92,9 @@ if __name__ == '__main__':
 
     # Shape: (T,)
     final = audio.squeeze(0)
-
+    print("Audio shape:", final.shape)
+    print("Audio length:", final.shape[0])
+    
     feat = extractor.extract(final)
     print(feat.shape)
     print(feat)
